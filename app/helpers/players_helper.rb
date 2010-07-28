f4498f58745fe8f7d626cf1d35cb5a72f08961d7
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2010-07-28 12:16:02 +0200

    added extended song information in playlist

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -6,4 +6,18 @@ module PlayersHelper
       song[:artist] + ' - ' + song[:title]
     end 
   end
+
+  def extended_song_info(song)
+    output = ''
+    if song.include?(:album)
+      output = content_tag(:strong,'Album:') + song[:album]
+    end
+    if song.include?(:genre)
+      output += content_tag(:strong,' Genre:') + song[:genre]
+    end
+    if song.include?(:date)
+      output += content_tag(:strong,' Date:')  + song[:date]
+    end
+    output
+  end
 end
