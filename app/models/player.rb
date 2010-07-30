77ee4109422bd9d60b4f3e09b98d36bdcc92eb54
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2010-07-30 11:17:55 +0200

    added singleton methods to Player class

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,19 +1,29 @@
 class Player
   extend ActiveModel::Naming
-
-  delegate :stop, :start, :pause, :paused?, :next,:previous,:current_song,:listall,:ping, :to => :mpc
+  delegate :stop, :start, :pause, :paused?, :next,:previous,:current_song,:ping, :to => :mpc
   attr_reader :mpc
+  @@instance = nil
 
   def initialize(host='localhost',port=6600)
     @mpc = Mpc.new(host,port)
+    @@instance = self
+  end
+
+  def self.instance
+    @@instance = new unless @@instance
+    @@instance
   end
 
   def play(song=nil)
     @mpc.play(song)
   end
-  
+
   def find(type,what="")
     @mpc.find(type,what)
+  end
+
+  def listall
+    @mpc.listall.map{|attributes| Song.new(@@instance,attributes)}
   end
 
 end
